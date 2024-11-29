4ecbccf50b5862a184a06ac1674d3d04c47a8cd9
Micro-88 <xXgen88Xx@gmail.com>
2024-11-29 12:34:28 +0800

    optimized backend database

--- app/database/migrations/3-workoutPlans.cjs
+++ app/database/migrations/3-workoutPlans.cjs
@@ -24,6 +24,24 @@ module.exports = {
           onUpdate: 'CASCADE',
           onDelete: 'CASCADE', // Deletes the workout plan if the user is deleted
         },
+        workoutId: {
+          type: Sequelize.INTEGER.UNSIGNED,
+          allowNull: false,
+          references: {
+            model: 'Workouts',
+            key: 'id',
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+        },
+        name: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        description: {
+          type: Sequelize.TEXT,
+          allowNull: true,
+        },
         createdAt: {
           type: Sequelize.DATE,
           allowNull: false,
