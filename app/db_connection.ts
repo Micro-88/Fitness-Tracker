53ca9a1465b7f692130c37e3dd9d2e7601bfa873
Micro-88 <xXgen88Xx@gmail.com>
2024-12-13 15:33:41 +0800

    resolved eslint issues

--- app/db_connection.ts
+++ app/db_connection.ts
@@ -1,9 +1,10 @@
 import { Sequelize } from "sequelize";
 import { SequelizeOptions } from "sequelize-typescript";
 import { options } from "./database/config/config.mjs"; 
+import mariadb from "mariadb"; // Import mariadb directly
 
 const dbOptions = <SequelizeOptions>options
-dbOptions.dialectModule = require('mariadb');
+dbOptions.dialectModule = mariadb;
 
 const sequelize = new Sequelize(dbOptions);
 
