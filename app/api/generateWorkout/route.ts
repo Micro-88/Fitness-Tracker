ecb502fc74a96aa1eea266c90c850a6cf3f2b9e8
Micro-88 <xXgen88Xx@gmail.com>
2024-12-14 16:36:13 +0800

    Fixed problem with calories burned

--- app/api/generateWorkout/route.ts
+++ app/api/generateWorkout/route.ts
@@ -64,7 +64,7 @@ export async function POST(req: NextRequest) {
       "intensity": "intensity",\n
       "instructions": "instructions",\n
       "description": "description", \n
-      "caloriesBurned": 0, \n
+      "caloriesBurned": "caloriesBurned", \n
       "isCompleted": false\n
       }\n]`;
 
